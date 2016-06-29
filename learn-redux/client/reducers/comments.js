6d0c975c9e6f2071d83a87f3b5f8c9cd02bcccec
Matt szabo <wekilledit@users.noreply.github.com>
2016-06-29 13:07:53 +1000

    like and comment functionality working

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,4 +1,29 @@
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      const i = action.i;
+      return [
+        ...state.slice(0,i),
+        ...state.slice(i+1)
+      ]
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      // pass a slice of state (comments for our postId) to a more specific reducer
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
