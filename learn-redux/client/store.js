93d1aee909ef02a99d54b53169f9c69f1fbc1eae
Matt szabo <wekilledit@users.noreply.github.com>
2016-06-29 13:51:05 +1000

    hot reload for reducers!

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -18,4 +18,12 @@ const store = createStore(rootReducer, defaultState);
 
 const history = syncHistoryWithStore(browserHistory, store);
 
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    // can't use es6 import statement inside of a function
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
 export {store as default, history};
